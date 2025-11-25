91cad110892b114382756b3f3c68a511224a27a6
kaylaknight385 <kaylaknight385@gmail.com>
2025-11-25 11:47:52 -0500

    finished Utils  folders. period.

--- utils/errorHandler.ts
+++ utils/errorHandler.ts
@@ -1,6 +1,21 @@
-export function handleError(error: unknown): string {
-  if (error instanceof Error) {
-    return error.message;
+//iggght lets handle errors...customly...
+
+export class ApiError extends Error {
+  constructor(message: string) {
+  super(message);
+  this.name= "APIERROR!"
   }
-  return 'An unknown error occurred';
 }
+
+
+//functions handles errors and logs them. tea.
+export function handleError(error: unknown): void {
+  if (error instanceof ApiError) {
+    console.error(`Damn...API ERROR: ${error.message}`);
+  } else if (error instanceof Error) {
+    console.error(`Error: ${error.message}`);
+  } else {
+    console.error("uhhhh lol idk unknown error occured")
+  }
+}
+//That whole block is for errors. If the error is a instance of a APIError then load ApiError message and so on...
--- utils/taxCalculator.ts
+++ utils/taxCalculator.ts
@@ -1,3 +1,11 @@
-export function calculateTax(price: number, taxRate: number): number {
-  return price * (1 + taxRate / 100);
+export function calculateTax(price: number, category: string): number {
+  //okey this is the tax cal. different from the discount cal cus it calculates taxes. */
+  if (category.toLowerCase() === "groceries") {
+    // .toLowerCase() makes "Groceries", "GROCERIES", "groceries" all work
+    return (price * 3) / 100;
+    // now it returns the tax cal price. Groceries are taxed 3%. So the price times 3 divided by 100 */
+  }
+  return (price * 4.75) / 100;
+  // nooooow this is for non groceries. they are taxed 4.75. So anything besides groceries will be returned with this function. */
+
 }
